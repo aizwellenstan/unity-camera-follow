43231a7e3c96b5c6c0cdc1050ab90f2d4bdb42b3
Saitou <saitou@example.com>
2020-05-26 11:31:52 +0900

    add following player

--- CameraFollow/GameHandler.cs
+++ CameraFollow/GameHandler.cs
@@ -4,9 +4,14 @@ public class GameHandler : MonoBehaviour
 {
     public CameraFollow cameraFollow;
 
+    public Transform playerTransform; // Target
+
     private void Start()
     {
         // cameraFollow.Setup(new Vector3(0, -100)); // Looking Down
-        cameraFollow.Setup(() => new Vector3(0, -100));
+        
+        // cameraFollow.Setup(() => new Vector3(0, -100));
+
+        cameraFollow.Setup(() => playerTransform.position);
     }
 }
