86689e0582d212f5fc71ec8906953fc305e3ae04
Saitou <saitou@example.com>
2020-05-27 15:53:19 +0900

    add OnDrawGizmos

--- TopDownCamera/Code/Scripts/IP_TopDown_Camera.cs
+++ TopDownCamera/Code/Scripts/IP_TopDown_Camera.cs
@@ -5,11 +5,24 @@ using UnityEngine;
 public class IP_TopDown_Camera : MonoBehehaviour
 {
 	public Transform m_Target;
-	public float m_Height = 10f;
-	public float m_Distance = 20f;
-	public float m_Angle = 45f;
 	
+	//public float m_Height = 10f;
+	[SerializeField]
+	private float m_Height = 10f;
 
+	// public float m_Distance = 20f;
+	[SerializeField]
+	private float m_Distance = 20f;
+
+	// public float m_Angle = 45f;
+	[SerializeField]
+	private float m_Angle = 45f;
+
+	// public float m_SmoothSpeed = 0.5f;
+	[SerializeField]
+	private float m_SmoothSpeed = 0.5f;
+
+	private Vector3 refVelocity;
 
 	void Start()
 	{
@@ -39,7 +52,19 @@ public class IP_TopDown_Camera : MonoBehehaviour
 		Vector3 finalPosition = flatTargetPosition + rotatedVector;
 		Debug.DrawLine(m_Target.position, finalPosition, Color.blue);
 
-		transform.position = finalPosition;
+		// transform.position = finalPosition;
+		transform.position = Vector3.SmoothDamp(transform.position, finalPosition, ref refVelocity, m_SmoothSpeed);
 		transform.LookAt(flatTargetPositon);
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = new Color(0f, 1f, 0f, 0.25f);
+		if(m_Target)
+		{
+			Gizmos.DrawLine(transform.position, m_Target.position);
+			Gizmos.DrawSphere(m_Target.position, 1.5f);
+		}
+		Gizmos.DrawSphere(transform.position, 1.5f);
 	}
 }
